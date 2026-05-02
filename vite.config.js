a5a88f21a5e7b1f456736a25eb7a1266637d08aa
Sesamakita <deniindrayana@gmail.com>
2026-05-02 14:19:37 +0800

    chore: add deployment configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Konfigurasi khusus untuk pengembangan Lokal
 export default defineConfig({
   plugins: [react()],
-  // base dihapus agar berjalan di http://localhost:5173/
+  base: '/epad_mobile_react/',
 })
